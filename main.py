c6c0d03df78f7779618314043365f880ac05f53e
gauri mali <lalitamali6789@gamil.com>
2025-06-19 17:29:33 +0530

    todo list

---
+++ main.py
@@ -0,0 +1,41 @@
+
+
+print("*"*25)
+print("Welcome to do list!")
+print("*"*25)
+from root import options,  list_task
+a=100
+task= [
+    { 'task' : "reading" , 'done' : False,}
+    ] 
+while(a!=99):
+    options()
+    a=int(input("Select option = "))
+    if(a==1):
+        print("-"*25)
+        list_task(task)
+        print("-"*25)
+    elif(a==2):
+        t = input("Enter task=")
+        task.append({'task':t,'done': False})
+        list_task(task)
+        print("-"*25)
+        
+    elif(a==3):
+        t = int(input("Enter task id to Delete task="))
+        task.pop(t)
+        list_task(task)
+    elif(a==4):
+        t=int(input("Enter task id to complete="))
+        task[t]['done'] = True
+        list_task(task)
+    else:
+        print("Exit")
+    
+        
+
+    
+
+
+
+
---
+++ root.py
@@ -0,0 +1,19 @@
+def options():
+    print("1.List all task")
+    print("2.Add to task")
+    print("3.Delete a task")
+    print("4.Mark as task Complete")
+    print("99. Exit")
+    print("-"*25)
+
+def list_task(task):
+    for i in task:
+        if i['done']:
+            print(task.index(i),i['task'] ,"------>", "Complete")
+        else:
+            print(task.index(i),i['task'] ,"------>", "InComplete")
+            
+        
+        
+        
+        
